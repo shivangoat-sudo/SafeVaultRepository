91746eca561ba6679108bd136f1ca48a9b08b706
shivangoat-sudo <shivangoat@gmail.com>
2026-09-01 14:23:52 +0200

    fix: route fiscal frontend boundary to strict engine v2

--- src/lib/btwFiscalSafeNormalized.ts
+++ src/lib/btwFiscalSafeNormalized.ts
@@ -1,2 +1,2 @@
-export { calculateFiscalVatReport, tweeKolommenWeergave, berekenBetrouwbaarheidsscore, BOEKHOUDER_PERCENTAGE_OPTIES } from './btwFiscalSafe';
-export type { FiscalReport, BoekhouderBeoordeling, FiscalClassification, FiscalSection, FiscalTransaction, BtwPercentage } from './btwFiscalSafe';
+export { calculateFiscalVatReport, tweeKolommenWeergave, berekenBetrouwbaarheidsscore, BOEKHOUDER_PERCENTAGE_OPTIES } from './btwFiscalSafeV2';
+export type { FiscalReport, BoekhouderBeoordeling, FiscalClassification, FiscalSection, FiscalTransaction, BtwPercentage } from './btwFiscalSafeV2';
