852485e89f6640302cc9a2596b1bb7072cf7c99f
shivangoat-sudo <shivangoat@gmail.com>
2026-09-03 15:48:02 +0200

    test: align edge cases and remove frontend diff

--- scripts/btwFiscalEdgeCases.ts
+++ scripts/btwFiscalEdgeCases.ts
@@ -8,5 +8,5 @@ assert.equal(reverse9.aangifte['4b'].btw,9); assert.equal(reverse9.aangifte['5b'
 const adjustment=calculateFiscalVatReport([],{}, {rubriek1c:{grondslag:100,btw:13},rubriek1d:{grondslag:500,btw:105}});
 assert.equal(adjustment.aangifte['1c'].btw,13); assert.equal(adjustment.aangifte['1d'].btw,105); assert.equal(adjustment.aangifte['5a'],118); assert.equal(adjustment.overzicht.netto,118); assert.equal(adjustment.audit.ok,true);
 assert.throws(()=>calculateFiscalVatReport([{id:'bad',type:'income',amount_incl:100,description:'bad'}],{bad:{classificatie:'other_rate_output',percentage:21,beoordeeld_door:'Boekhouder'}}));
-assert.throws(()=>calculateFiscalVatReport([{id:'bad',type:'expense',amount_incl:100,description:'bad'}],{bad:{classificatie:'eu_reverse_charge',percentage:13,beoordeeld_door:'Boekhouder'}}));
+assert.throws(()=>calculateFiscalVatReport([{id:'bad',type:'income',amount_incl:100,description:'bad'}],{bad:{classificatie:'eu_reverse_charge',percentage:13,beoordeeld_door:'Boekhouder'}}));
 console.log('BTW fiscal edge-case tests passed');
